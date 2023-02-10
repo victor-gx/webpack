ac2fce1f72466c9f6fe5f00750a8e13c6f42fc2e
gx <2416457672@qq.com>
2023-02-10 23:48:02 +0800

    基础

---
+++ 基础/webpack_code/src/main.js
@@ -0,0 +1,12 @@
+import count from "./js/count";
+import sum from "./js/sum";
+// 引入资源，Webpack才会对其打包
+import "./css/index.css";
+import "./less/index.less";
+import "./sass/index.sass";
+import "./sass/index.scss";
+import "./styl/index.styl";
+
+const result = count(2, 2);
+console.log(result);
+console.log(sum(1, 2, 3, 4));
